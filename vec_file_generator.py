9a471e252da10d3f101fdff7a8baa2414a5274d8
alonkamir <alonk147@gmail.com>
2021-05-03 20:32:58 +0300

    removing main

--- vec_file_generator.py
+++ vec_file_generator.py
@@ -3,14 +3,26 @@ class VecFileGenerator(object):
         self.output_file_name = output_file_name
     
     def one_bias(self, bias_location, number_of_non_bias_pgs):
-        number_of_pgs = 5
         base = 1 << (number_of_non_bias_pgs - bias_location)
         highest_value = 2**(number_of_non_bias_pgs + 1) - 1
         config_set = {base}  
         for i in range(highest_value):
             config_set.add(base | i)
         self._write_to_file(config_set, number_of_non_bias_pgs + 1)
-        
+
+    def multiple_biases_one_level(self, bias_location_list, number_of_non_bias_pgs):
+        total_number_of_pgs = len(bias_location_list) + number_of_non_bias_pgs
+        highest_value = 2**(total_number_of_pgs) - 1
+        config_set = set()
+        for bias_location in bias_location_list:
+            base = 1 << (total_number_of_pgs - 1 - bias_location)
+            for i in range(highest_value):
+                config_set.add(base | i)
+        # for x in config_set:
+        #     print(f'{x:06b}')
+        self._write_to_file(config_set, number_of_non_bias_pgs + len(bias_location_list))
+
+
 
     def _write_to_file(self, config_set, total_number_of_pgs):
         try:
@@ -19,10 +31,3 @@ class VecFileGenerator(object):
             raise Exception(f'Could not open file: {e}')
         for config in config_set:
             vec_file.write(f'{config:0{total_number_of_pgs}b}\n')
-
-def main():
-    VecFileGenerator('5_pg.txt').one_bias(bias_location=0, number_of_non_bias_pgs=4)
-
-
-if __name__ == "__main__":
-    main()
