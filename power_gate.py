f87bfc9ab47272cad8f9fc18f103d806d8594b27
alonkamir <alonk147@gmail.com>
2021-05-03 20:39:15 +0300

    changing res in constructor and calculations

--- power_gate.py
+++ power_gate.py
@@ -1,21 +1,15 @@
 class PowerGate(object):
     pg_id = 0
     all_pgs = {}
-    def __init__(self, size, res=None, bias=False, name=None):
+    def __init__(self, res, size=None, bias=False, name=None):
         if name == None:
             self.id = str(PowerGate.pg_id)
             PowerGate.pg_id += 1
             PowerGate.all_pgs[self.id] = self
         else:
             self.id = name
-        self.size = size
-        if res == None:
-            if size == 0:
-                self.res = 0
-            else:
-                self.res = 1/size
-        else:
-            self.res = res
+        self.size = 0
+        self.res = res
         if bias:
             self.color = 'orange'
         else:
@@ -23,7 +17,7 @@ class PowerGate(object):
         
     # Connect PGs in series
     def __and__(self, x):
-        return PowerGate(self.size + x.size, self.res + x.res)
+        return PowerGate(self.res + x.res, self.size + x.size)
     
     # Connect PGs in parallel
     def __or__(self, x):
@@ -34,7 +28,7 @@ class PowerGate(object):
                 new_res = self.res
             else:
                 new_res = x.res
-        return PowerGate(self.size + x.size, new_res)
+        return PowerGate(new_res, self.size + x.size)
     def __str__(self):
         return f"Size: {self.size}, Resistance: {self.res}"
 
@@ -48,3 +42,11 @@ class PowerGate(object):
         else:
             pg.res = new_pg.res
             pg.size = new_pg.size
+
+    def get_pg_area(size):
+        area_dict = {'0.5': 16, '1': 8, '2': 4, '4': 2, '8': 1, '16': 2, '32': 4, '64': 8, '128': 16}
+        if area_dict.get(size, None) == None:
+            print("Size not in dict")
+            return 0
+        else:
+            return area_dict.get(size)
